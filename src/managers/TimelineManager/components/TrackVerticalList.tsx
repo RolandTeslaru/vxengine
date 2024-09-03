0597150edeb2a38416b8b40dad427ed86e95b5bb
Roland Teslaru <89258659+RolandTeslaru@users.noreply.github.com>
2024-09-03 21:07:10 +0300

    collapsible tracks

--- src/managers/TimelineManager/components/TrackVerticalList.tsx
+++ src/managers/TimelineManager/components/TrackVerticalList.tsx
@@ -2,7 +2,7 @@
 // (c) 2024 VEXR Labs. All Rights Reserved.
 // See the LICENSE file in the root directory of this source tree for licensing information.
 
-import { ChevronLeft, Square, ChevronRight } from "lucide-react";
+import { ChevronLeft, Square, ChevronRight, ChevronDown } from "lucide-react";
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { edObjectProps, ITrack } from "vxengine/AnimationEngine/types/track";
 import { useVXEngine } from "vxengine/engine";
@@ -45,23 +45,61 @@ const KeyframeControl = React.memo(({ track, propertyPath, edObject }: { track: 
             <button onClick={() => moveToPreviousKeyframe(animationEngine, edObject.vxkey, propertyPath)} className='hover:*:stroke-[5] hover:*:stroke-white'>
                 <ChevronLeft className=' w-3 h-3' />
             </button>
-            <button onClick={createNewKeyframe} className="hover:*:stroke-[5] hover:*:stroke-white ">
+            <button onClick={() => createNewKeyframe(animationEngine, edObject.vxkey, propertyPath, 0)} className="hover:*:stroke-[5] hover:*:stroke-white ">
                 <Square className={`rotate-45 w-2 h-2 ${isOnKeyframe ? "fill-blue-500 stroke-blue-400 scale-110" : ""}`} />
             </button>
-            <button onClick={moveToNextKeyframe} className='hover:*:stroke-[5] hover:*:stroke-white'>
+            <button onClick={() => moveToNextKeyframe(animationEngine, edObject.vxkey, propertyPath)} className='hover:*:stroke-[5] hover:*:stroke-white'>
                 <ChevronRight className='w-3 h-3 ' />
             </button>
         </div>
     )
 });
 
+interface TrackGroup {
+    children: Record<string, TrackGroup>;
+    track: ITrack | null;
+    rowIndex?: number;
+    prevRowIndex?: number;
+    nextRowIndex?: number;
+    finalRowIndex?: number;
+}
 const TrackVerticalList = () => {
-    const editorData = useTimelineEditorStore(state => state.editorData)
-    const objects = useVXObjectStore(state => state.objects)
+    const [collapsedGroups, setCollapsedGroups] = useState<Record<string, boolean>>({});
 
-    // Helper function to group tracks by their parent keys
+    const toggleCollapse = (groupKey: string) => {
+        setCollapsedGroups(prevState => ({
+            ...prevState,
+            [groupKey]: !prevState[groupKey],
+        }));
+    };
+
+    const editorData = useTimelineEditorStore(state => state.editorData);
+    const objects = useVXObjectStore(state => state.objects);
+
+    const precomputeRowIndices = (groupedTracks: Record<string, TrackGroup>, currentRowIndex = 1, prevRowIndex = 0): number => {
+        Object.entries(groupedTracks).forEach(([key, group]) => {
+            const hasChildren = group.children && Object.keys(group.children).length > 0;
+            const hasMultipleChildren = group.children && Object.keys(group.children).length > 1;
+            const isFinalValue = !hasChildren && group.track;
+    
+            group.prevRowIndex = prevRowIndex;
+            group.rowIndex = currentRowIndex;
+            group.nextRowIndex = hasMultipleChildren ? currentRowIndex + 1 : currentRowIndex;
+    
+            if (hasChildren) {
+                const childFinalIndex = precomputeRowIndices(group.children, group.nextRowIndex, group.rowIndex);
+                group.finalRowIndex = childFinalIndex;
+            } else if (isFinalValue) {
+                group.nextRowIndex = currentRowIndex + 1;
+                group.finalRowIndex = group.nextRowIndex - 1;
+            }
+        });
+    
+        return groupedTracks[Object.keys(groupedTracks)[Object.keys(groupedTracks).length - 1]]?.finalRowIndex || currentRowIndex;
+    };
+
     const groupTracksByParent = (tracks: ITrack[]) => {
-        const groupedTracks: Record<string, any> = {};
+        const groupedTracks: Record<string, TrackGroup> = {};
 
         tracks.forEach((track) => {
             const pathSegments = track.propertyPath.split('.');
@@ -70,7 +108,7 @@ const TrackVerticalList = () => {
 
             pathSegments.forEach((key, index) => {
                 if (!currentGroup[key]) {
-                    currentGroup[key] = { children: {}, track: null, index: index + 1};
+                    currentGroup[key] = { children: {}, track: null, index: index + 1 };
                 }
 
                 if (index === pathSegments.length - 1) {
@@ -80,64 +118,82 @@ const TrackVerticalList = () => {
                 }
             });
         });
+        precomputeRowIndices(groupedTracks);
 
         return groupedTracks;
     };
 
     const renderGroupedTracks = (
-        groupedTracks: Record<string, any>,
+        groupedTracks: Record<string, TrackGroup>,
         edObject: edObjectProps,
         depth = 1,
         shouldIndent = false,
-        currentRowIndex,
-        prevRowIndex,
+        parentPath = ''
     ) => {
         return Object.entries(groupedTracks).map(([key, group]) => {
             const hasChildren = group.children && Object.keys(group.children).length > 0;
             const hasMultipleChildren = group.children && Object.keys(group.children).length > 1;
-
+    
             const isFinalGroup = hasChildren && Object.values(group.children).every(
-                (child: any) => child.track && (!child.children || Object.keys(child.children).length === 0)
+                (child: TrackGroup) => child.track && (!child.children || Object.keys(child.children).length === 0)
             );
-
-            // Determine if padding should be added based on sibling relationships
+    
             const shouldIndentChildren = hasMultipleChildren && !group.track && !isFinalGroup;
-
-            const isFinalValue = hasChildren
-
-            // Check if current group has children that are not just single tracks
-
-
-            // Update rowIndex if there are children
-            const nextRowIndex = hasMultipleChildren ? currentRowIndex + 1 : currentRowIndex;
-
-
-       
-            const isCollapsible = (currentRowIndex !== prevRowIndex) && isFinalValue
-            // console.log("Rendering key:", key, " hasChildren:", hasChildren, "currentRowIndex:", currentRowIndex)
-
+    
+            const isFinalValue = !!group.track;
+    
+            const isCollapsible = (group.rowIndex !== group.prevRowIndex) && (group.rowIndex !== group.finalRowIndex) && !isFinalValue;
+    
+            const groupKey = `${parentPath}/${key}`;
+    
+            const isCollapsed = collapsedGroups[groupKey] || false;
+    
+            // Create a combined track when the group is collapsed
+            let combinedTrack: ITrack | null = null;
+            if (isCollapsed && hasChildren) {
+                const combinedKeyframes = [];
+                const collectKeyframes = (trackGroup: Record<string, TrackGroup>) => {
+                    Object.values(trackGroup).forEach(child => {
+                        if (child.track) {
+                            combinedKeyframes.push(...child.track.keyframes);
+                        }
+                        if (child.children) {
+                            collectKeyframes(child.children);
+                        }
+                    });
+                };
+                collectKeyframes(group.children);
+                combinedTrack = { propertyPath: groupKey, keyframes: combinedKeyframes };
+            }
+    
+            console.log("Rendering key:", key, " hasChildren:", hasChildren, "currentRowIndex:", group.rowIndex, " isFinalValue:", isFinalValue, " group:", group);
+    
             return (
                 <div
-                    key={key}
+                    key={groupKey}
                     className={`${key} w-full flex ${hasMultipleChildren ? "flex-col" : "flex-row"}`}
-                    style={{ paddingLeft: shouldIndent && 16 }}
+                    style={{ paddingLeft: shouldIndent ? 16 : 0 }}
                 >
-                    <div className={`h-8 flex items-center`} style={{ marginLeft: group.track ? "auto" : undefined }}>
-                        {/* Collapse Button Chvron */}
-
+                    <div className={`h-8 flex items-center`} style={{ marginLeft: isFinalValue ? "auto" : undefined }}>
                         {isCollapsible &&
-                            <div>
-                                <ChevronRight />
+                            <div onClick={() => toggleCollapse(groupKey)}>
+                                {isCollapsed ? <ChevronRight /> : <ChevronDown />}
                             </div>
                         }
-                        <p className={`${group.track ? 'text-neutral-500' : 'font-bold'} mr-2`}>
+                        <p className={`${isFinalValue ? 'text-neutral-500' : ' '} mr-2`}>
                             {key}
                         </p>
-                        {group.track && (
+                        {/* Render KeyframeControl for collapsed groups */}
+                        {isCollapsed && combinedTrack && (
+                            <div className="w-auto ml-auto">
+                                <KeyframeControl track={combinedTrack} propertyPath={combinedTrack.propertyPath} edObject={edObject} />
+                            </div>
+                        )}
+                        {group.track && !isCollapsed && (
                             <KeyframeControl track={group.track} propertyPath={group.track.propertyPath} edObject={edObject} />
                         )}
                     </div>
-                    {!group.track && hasChildren && renderGroupedTracks(group.children, edObject, depth + 1, shouldIndentChildren, nextRowIndex, currentRowIndex)}
+                    {!group.track && hasChildren && !isCollapsed && renderGroupedTracks(group.children, edObject, depth + 1, shouldIndentChildren, groupKey)}
                 </div>
             );
         });
@@ -150,15 +206,15 @@ const TrackVerticalList = () => {
                 const object = objects[vxkey];
                 const groupedTracks = groupTracksByParent(tracks);
 
-                console.log("Group TrackBy Parent ", groupedTracks)
+                console.log("Group TrackBy Parent ", groupedTracks);
 
                 return (
                     <div key={vxkey} className="flex flex-col ">
-                        <div className='h-8 bg-neutral-900 flex'>
+                        <div className='h-8 flex'>
                             <p className="font-bold h-auto my-auto text-white">{object?.name}</p>
                         </div>
                         <div className="flex flex-col rounded-md">
-                            {renderGroupedTracks(groupedTracks, edObject, 0, true, 1, 0)}
+                            {renderGroupedTracks(groupedTracks, edObject, 0, true, vxkey)} {/* Start the path with the object's key */}
                         </div>
                     </div>
                 );
@@ -166,5 +222,4 @@ const TrackVerticalList = () => {
         </div>
     );
 };
-
-export default TrackVerticalList
+export default TrackVerticalList;
